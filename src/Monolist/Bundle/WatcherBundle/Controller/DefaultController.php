9ec6b890d684986aabde5582bde2bfb5054c47b0
Tino Stöckel <tino.stoeckel@fashionforhome.de>
2014-10-22 13:27:37 +0200

    Adapt routing to enable a default routing for the watcher bundle

--- src/Monolist/Bundle/WatcherBundle/Controller/DefaultController.php
+++ src/Monolist/Bundle/WatcherBundle/Controller/DefaultController.php
@@ -3,10 +3,22 @@
 namespace Monolist\Bundle\WatcherBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
+    {
+        return $this->render('MonolistWatcherBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    /**
+     * @Route("/hello/{name}", name="_default_hello")
+     * @Template()
+     */
+    public function helloAction($name)
     {
         return $this->render('MonolistWatcherBundle:Default:index.html.twig', array('name' => $name));
     }
