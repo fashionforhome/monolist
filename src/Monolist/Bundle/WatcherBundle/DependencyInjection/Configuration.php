4aa849b15fd725a3e27a6a6f446fb4f103cf178e
Rebel-L <dj@rebel-l.net>
2015-10-30 02:22:23 +0100

    added license information to dependency injection

--- src/Monolist/Bundle/WatcherBundle/DependencyInjection/Configuration.php
+++ src/Monolist/Bundle/WatcherBundle/DependencyInjection/Configuration.php
@@ -1,15 +1,28 @@
 <?php
-
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ *
+ * @category Monolist
+ * @package WatcherBundle
+ *
+ * @author Tino Stöckel <tino.stoeckel@fashionforhome.de>
+ *
+ * @copyright (c) 2014 by fashion4home GmbH <www.fashionforhome.de>
+ * @license GPL-3.0
+ * @license http://opensource.org/licenses/GPL-3.0 GNU GENERAL PUBLIC LICENSE
+ *
+ * @version 1.0.0
+ *
+ * Date: 21.10.2014
+ * Time: 22:04
+ */
 namespace Monolist\Bundle\WatcherBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * This is the class that validates and merges configuration from your app/config files
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
- */
 class Configuration implements ConfigurationInterface
 {
     /**
