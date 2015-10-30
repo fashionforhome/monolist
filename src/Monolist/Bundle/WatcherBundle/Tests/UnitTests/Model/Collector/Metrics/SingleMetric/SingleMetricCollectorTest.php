3964f5127abab669ac900f3f2f382386b0a5a19b
Rebel-L <dj@rebel-l.net>
2015-10-30 02:33:34 +0100

    added license information to tests

--- src/Monolist/Bundle/WatcherBundle/Tests/UnitTests/Model/Collector/Metrics/SingleMetric/SingleMetricCollectorTest.php
+++ src/Monolist/Bundle/WatcherBundle/Tests/UnitTests/Model/Collector/Metrics/SingleMetric/SingleMetricCollectorTest.php
@@ -1,4 +1,21 @@
 <?php
+/**
+ * This is the ..... [Description]
+ *
+ * @category Monolist
+ * @package WatcherBundle
+ *
+ * @author Tino Stöckel <tino.stoeckel@fashionforhome.de>
+ *
+ * @copyright (c) 2014 by fashion4home GmbH <www.fashionforhome.de>
+ * @license GPL-3.0
+ * @license http://opensource.org/licenses/GPL-3.0 GNU GENERAL PUBLIC LICENSE
+ *
+ * @version 1.0.0
+ *
+ * Date: 23.10.2014
+ * Time: 22:04
+ */
 namespace Monolist\Bundle\WatcherBundle\Tests\UnitTests\Model\Collector\Metrics\SingleMetric;
 
 
